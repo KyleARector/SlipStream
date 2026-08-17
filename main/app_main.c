de39864fce7155a217bf214b6c234413cec1a69f
Kyle Rector <kylearector@outlook.com>
2026-08-17 09:14:23 -0400

    Adding basic app, sdkconfig, and .gitignore

---
+++ main/app_main.c
@@ -0,0 +1,13 @@
+#include "esp_idf_version.h"
+#include "esp_log.h"
+
+static const char *TAG = "slipstream";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "================================================");
+    ESP_LOGI(TAG, " SlipStream firmware");
+    ESP_LOGI(TAG, " Build: %s %s", __DATE__, __TIME__);
+    ESP_LOGI(TAG, " ESP-IDF: %s", esp_get_idf_version());
+    ESP_LOGI(TAG, "================================================");
+}
